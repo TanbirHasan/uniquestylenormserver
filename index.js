67a9722e534585b566514fa252f5f220ac4733e0
unknown <mdtanbir576@gmail.com>
2022-07-08 21:59:29 +0600

    order end poitn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-
+const stripe = require("stripe")(
+  "  sk_test_51L3crVI20gp0i97mRSUa6lJ5ZbgZAnYdK9cAoJ13JyFAXlVKpyKTm2F2Lb58MrCmc9GhUEWnW7nPprb3C2VelXbO00iJZ5Iefm"
+);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 9500;
 
@@ -52,7 +54,43 @@ async function run(){
         res.send(order);
       });
 
-     
+      // posting a order order
+      app.post("/order", async (req, res) => {
+        const newService = req.body;
+        const result = await ordercollection.insertOne(newService);
+        res.send(result);
+      });
+
+      // getting individual orders
+      app.get("/myorder", async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const cursor = ordercollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      });
+
+      // getting order for payment
+      app.get("/order/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const order = await ordercollection.findOne(query);
+        res.send(order);
+      });
+
+      // stripe payment
+      app.post("/create-payment-intent", async (req, res) => {
+        const service = req.body;
+        const price = service.price;
+        console.log(price)
+        const amount = price * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+        res.send({ clientSecret: paymentIntent.client_secret });
+      });
     }
     catch(err){
 
